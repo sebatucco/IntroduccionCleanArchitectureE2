c710ded7a447b860045143535c8805f1f8b0e79d
sebatucco <c.sebastian.ojeda@gmail.com>
2024-03-31 17:01:36 -0300

    dependency container incompleto

---
+++ IntroduccionCleanArchitectureE2.NorthWindInversionOfControl/DependencyContainer.cs
@@ -0,0 +1,23 @@
+﻿using IntroduccionCleanArchitectureE2.NorthWindEntities.Interfaces;
+using IntroduccionCleanArchitectureE2.NorthWindRepositoriesEFCore.DataContext;
+using IntroduccionCleanArchitectureE2.NorthWindRepositoriesEFCore.Repositories;
+using IntroduccionCleanArchitectureE2.NorthWindUseCases.CreateOrder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IntroduccionCleanArchitectureE2.NorthWindInversionOfControl
+{
+    public static class DependencyContainer
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration) 
+        {
+            services.AddDbContext<Context>(options => options.UseSqlServer(configuration.GetConnectionString("NorthWindDb")));
+            services.AddScoped<IOrderRepository, OrderRepository>();
+            services.AddScoped<IOrderDetailRepository, OrderDetailRepository>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            return services;
+        }
+    }
+}
