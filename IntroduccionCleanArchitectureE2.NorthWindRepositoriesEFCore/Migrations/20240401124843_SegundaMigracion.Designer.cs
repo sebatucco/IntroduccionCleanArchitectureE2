c85b1049f9652bb4ede7f8753921a8a0e5906901
sebatucco <c.sebastian.ojeda@gmail.com>
2024-04-01 09:49:35 -0300

    migraciones

--- IntroduccionCleanArchitectureE2.NorthWindRepositoriesEFCore/Migrations/20240401124843_SegundaMigracion.Designer.cs
+++ IntroduccionCleanArchitectureE2.NorthWindRepositoriesEFCore/Migrations/20240401124843_SegundaMigracion.Designer.cs
@@ -4,6 +4,7 @@ using IntroduccionCleanArchitectureE2.NorthWindRepositoriesEFCore.DataContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace IntroduccionCleanArchitectureE2.NorthWindRepositoriesEFCore.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20240401124843_SegundaMigracion")]
+    partial class SegundaMigracion
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
